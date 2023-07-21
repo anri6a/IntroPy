53bfbaf3f5dd9d65ff1b71895bc0376dff805e05
Andrei Shyrakou <anri6a@gmail.com>
2023-07-21 11:33:35 +0300

    add chapter Strings from book

---
+++ Training/Strings.py
@@ -0,0 +1,15 @@
+name = "hi there"
+first_name = "andrei"
+second_name = "shyrakou"
+full_name = first_name + " " +second_name #concatenate
+print(name.title()) #Заглавные в каждом слове
+print(name.lower()) #строчные ВСЕ
+print(name.upper()) #заглавные ВСЕ 
+print('Hello, ' + full_name.title() + "!") #concatenate
+print('Python')
+print('\tPython')
+print('\tPython is \n\tgood language')
+strWithSpaces = ' string with spaces '
+strWithSpaces.rstrip() #удаление пробелов в конце строки
+strWithSpaces.lstrip() #удаление пробелов в начале строки
+strWithSpaces.strip #удаление пробелов в начале и конце строки
